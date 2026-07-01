acffe64d352a10eae1263882d622f10769250243
JaimeLxrd <jf847852208@gmail.com>
2026-07-01 22:57:52 +0200

    adcionando variaveis globais

--- src/main/java/com/dev/lxrd/AmanhaEuFaco/controllers/WhatsAppWebhookController.java
+++ src/main/java/com/dev/lxrd/AmanhaEuFaco/controllers/WhatsAppWebhookController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
-import lombok.Value;
 
 @RestController
 @RequestMapping("/webhook")
